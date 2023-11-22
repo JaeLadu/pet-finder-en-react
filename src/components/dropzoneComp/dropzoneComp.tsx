89702c3c5992f55e803cf2b030604dd54855e19a
Jae <jaeladu1@gmail.com>
2023-11-22 11:01:27 -0300

    Created dropzoneComp and made visual and functional improvements to it

---
+++ src/components/dropzoneComp/dropzoneComp.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import css from "./dropzoneComp.css";
+import Dropzone from "react-dropzone";
+import { dropzone, img, thumbnail, thumbnailContainer } from "./dropzoneStyles";
+
+type props = {
+   handleFile: (file: any) => any;
+};
+export function DropzoneComp({ handleFile }: props) {
+   const [file, setFile] = useState({});
+   useEffect(() => {
+      // Necesario para que las thumbnails no generen errores en dropzone
+      return URL.revokeObjectURL(file.preview);
+   }, []);
+
+   return (
+      <div className={css.dropzone}>
+         <Dropzone
+            onDrop={async (acceptedFiles) => {
+               // gets image base64 string and other neccesary transformations for de component to work
+               const file = await parseFile(acceptedFiles[0]);
+               setFile(file);
+               //    function passed from the outside
+               handleFile(file);
+            }}
+         >
+            {({ getRootProps, getInputProps }) => (
+               <div
+                  {...getRootProps({
+                     className: "dropzone",
+                     style: dropzone,
+                  })}
+               >
+                  <input {...getInputProps()} />
+                  {file.dataURL ? (
+                     <div style={thumbnailContainer} key={file.name}>
+                        <div style={thumbnail}>
+                           <img
+                              src={file.preview}
+                              style={img}
+                              // Necesario para que las thumbnails no generen errores en dropzone
+                              onLoad={() => {
+                                 URL.revokeObjectURL(file.preview);
+                              }}
+                           />
+                        </div>
+                     </div>
+                  ) : (
+                     <span>Agregar foto</span>
+                  )}
+               </div>
+            )}
+         </Dropzone>
+      </div>
+   );
+}
+
+function getBase64(file: File | Blob) {
+   return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+         try {
+            resolve(reader.result?.toString()!);
+         } catch (error) {
+            console.log("error en load de fileReader", error);
+         }
+      };
+      reader.onerror = (error) => console.log("error de fileReader", error);
+   });
+}
+
+async function parseFile(file: File) {
+   const dataURL = await getBase64(file);
+   const preview = URL.createObjectURL(file);
+   const parsedFile = {
+      ...file,
+      dataURL: dataURL,
+      preview: preview,
+   };
+   return parsedFile;
+}
--- src/components/dropzoneComp/dropzoneStyles.ts
+++ src/components/dropzoneComp/dropzoneStyles.ts
@@ -4,7 +4,6 @@ export const dropzone = {
    flexDirection: "column",
    alignItems: "center",
    justifyContent: "center",
-   padding: "20px",
    borderWidth: 2,
    borderRadius: 2,
    borderColor: "#eeeeee",
@@ -20,18 +19,15 @@ export const dropzone = {
 export const thumbnailContainer = {
    display: "inline-flex",
    borderRadius: 2,
-   border: "1px solid #eaeaea",
-   marginBottom: 8,
-   marginRight: 8,
-   width: 100,
-   height: 100,
-   padding: 4,
+   width: "100%",
+   height: "100%",
    boxSizing: "border-box",
 };
 export const thumbnail = {
    display: "flex",
    minWidth: 0,
    overflow: "hidden",
+   justifyContent: "center",
 };
 export const img = {
    display: "block",
